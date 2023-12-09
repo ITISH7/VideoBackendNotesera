76acc660e2892c3e1524c7539bd99f1560fb164b
itish <itishjain7@gmail.com>
2023-12-10 03:07:40 +0530

    added authentication files

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const app =express()
+const bcrypt = require("bcrypt");
+const cookieparser = require("cookie-parser");
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const env = require('dotenv');
 env.config();
+const {notfound,errorhandler} =require('./middleware/errorhandler')
 const dbconnect = require('./config/dbconfig');
+//database connectivity
 dbconnect();
-const videoRouter = require('./routes/videodataRoute');
-const authRouter = require('./routes/authRoute');
+//routing  variables
+const video = require('./routes/videodataRoute');
+const auth = require('./routes/authRoute');
+//configures
 app.use(bodyParser.json())
 app.use(cors())
+app.use(cookieparser())
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/video',videoRouter);
-app.use('/authlogin',authRouter);
+//routing
+app.use('/authlogin',auth);
+app.use('/video',video);
+// middleware error handlings
+app.use(notfound);
+app.use(errorhandler);
 const Port = process.env.PORT||4000
 app.listen(Port,()=>{
     console.log(`server is running at${Port}`);
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,13 @@
-const { createuser, getuser } = require('../controller/authcontroller');
+const {createUser ,loginUser, getalluser, getuser, deleteuser, updateuser, handleRefreshToken, logout} = require("../controller/authcontroller");
+const { authMiddleware, isAdmin } = require("../middleware/authmiddleware");
+const router =  require("express").Router();
+router.post("/register",createUser)
+router.post("/login",loginUser)
+router.get("/refresh",handleRefreshToken)
+router.delete("/:id",deleteuser)
+router.get("/logout",logout)
+router.get("/user-details",authMiddleware,isAdmin,getalluser)
+router.get("/:id",authMiddleware,isAdmin,getuser);
+router.put("/edit-user",authMiddleware,updateuser)
 
-const route = require('express').Router();
-
-route.post('/addadmin',createuser);
-route.post('/admin',getuser)
-module.exports = route;
+module.exports=router;
